fd591496db244e9611df1bc38d87758afa3b2d5c
Ferran Piñol <49961065@est.ort.edu.ar>
2026-03-24 23:04:12 -0300

    Update Alumno.cs

--- Alumno.cs
+++ Alumno.cs
@@ -1,30 +1,42 @@
-namespace TP0._5; 
+namespace TP0._5;
 
 class Alumno
 {
-   private int DNI;
-   private string nombre;
-   private double cantidadfaltas;
+private int DNI;
+private string nombre;
+private double cantidadfaltas;
 
-   public Cuartel(int DNI,string nombre,double cantidadfaltas){
+public Alumno(int DNI, string nombre, double cantidadfaltas)
+{
+this.DNI = DNI;
+this.nombre = nombre;
+this.cantidadfaltas = cantidadfaltas;
+}
 
-   
+public int getDNI()
+{
+return this.DNI;
+}
 
-   this.DNI = DNI;
-   this.bomberos = bomberos;
-   this.cantidadfaltas = cantidadfaltas;
-   }
+public string getNombre()
+{
+return this.nombre;
+}
 
-   public int getDNI() {
+public double getFaltas()
+{
+return this.cantidadfaltas;
+}
 
-      return this.DNI;
+public void agregarFaltaCompleta()
+{
+this.cantidadfaltas = this.cantidadfaltas + 1;
+}
 
-   }
-   public double getFaltas() {
-
-      return this.faltas;
-
-   }
-
+public void agregarMediaFalta()
+{
+this.cantidadfaltas = this.cantidadfaltas + 0.5;
 
 }
+}
+
