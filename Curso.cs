b07330a0cc366ea25ece6bf1e1e4168b5f748b28
Ferran Piñol <49961065@est.ort.edu.ar>
2026-03-24 23:04:52 -0300

    Update Curso.cs

--- Curso.cs
+++ Curso.cs
@@ -1,95 +1,66 @@
-namespace TP0._5; 
+namespace TP0._5;
 
 class Curso
 {
-   private List<Alumno> Alumno;
+   private List<Alumno> alumnos;
 
    public Curso()
+   {
+       alumnos = new List<Alumno>();
+   }
+
+   public void agregaNuevoAlumno(int DNI, string nombre, double cantidadfaltas)
+   {
+       Alumno nuevo = new Alumno(DNI, nombre, cantidadfaltas);
+       alumnos.Add(nuevo);
+   }
+
+   public Alumno buscarAlumnoxdni(int DNI)
+   {
+       int i = 0;
+       Alumno encontrado = null;
+
+       while (i < alumnos.Count && alumnos[i].getDNI() != DNI)
+       {
+           i++;
+       }
+
+       if (i < alumnos.Count)
+       {
+           encontrado = alumnos[i];
+       }
+
+       return encontrado;
+   }
+
+   public string mostrarAlumnos()
+   {
+       string msg = "";
+
+       for (int i = 0; i < alumnos.Count; i++)
+       {
+        msg += "DNI: " + alumnos[i].getDNI() +
+" - Nombre: " + alumnos[i].getNombre() +
+" - Faltas: " + alumnos[i].getFaltas() + "\n";
+       }
+
+       return msg;
+   }
+
+public string AlumnosLibres()
 {
-
-   
-   
-}
-
- public Alumno agregaNuevoAlumno(int DNI, string nombre, double cantidadfaltas )
- {
-    Alumno alumnos = null;
-     int pos = 0;
-     while (pos < alumnos.Count())
-     if (pos < alumnos.Count())
-
-         alumnos = this.alumnos[pos];
-     return alumnos;
-   
-}
-}
-public Alumno buscarAlumnoxdni(int DNI)
-{
-
-    int i = 0;
-    Alumno encontrado = null;
-    while (i < this.alumnos.Count && DNI != alumnos[i].getDNI()) {
-
-        i++;
-
-    }
-    encontrado = alumnos[i];
-    return encontrado;
-}
-public int faltas()
-{
-
-
-int faltacompleta = 0;
-int mediafalta = 0;
-bool faltotodoeldia;
-bool faltosolounturno;
-
-if(faltotodoeldia)
-{
-  faltacompleta++;
-}
-else if(faltosolounturno)
-{
-   mediafalta++;
-   mediafalta + mediafalta = faltacompleta;
-}
-
-else
-{
-   mediafalta = false;
-   faltacompleta = false;
-}
-return faltas;
-}
-public string mostrarAlumnos() {
-
-    string msg = "";
-    for (int i = 0; i < alumnos.Count; i++) {
-
-        msg += alumnos[i] + ", \n";
-
-    }
-    return msg;
-
-}
-
-public string AlumnosLibres() {
-
-
 string msg = "";
-for(int i=0; i < alumnos.Count; i++) {
 
-    if (alumnos[i].getFaltas() > 15) {
-
-        msg += alumnos[i] + ", \n";
-
-    }
+for (int i = 0; i < alumnos.Count; i++)
+{
+if (alumnos[i].getFaltas() > 15)
+{
+msg += "DNI: " + alumnos[i].getDNI() +
+" - Nombre: " + alumnos[i].getNombre() +
+" - Faltas: " + alumnos[i].getFaltas() + "\n";
+}
 }
 
-
-
+return msg;
 }
-   
-
 
